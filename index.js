747aa369fb47e237d933b170f837d656fab767a6
Mochamad Fajar Sodik <mochamad.fajarsodik@gmail.com>
2024-06-20 15:20:35 +0700

    from javascript to react

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 const app = document.getElementById("app");
+
+function Header({ title }) {
+  return <h1>{title ? title : "default title"}</h1>;
+}
+
+function HomePage() {
+  const names = ["fajar", "sodik", "mochamad"];
+  function handleClick() {
+    setLikes(likes + 1);
+  }
+  const [likes, setLikes] = React.useState(0);
+  return (
+    <div>
+      {/* Nesting the Header component */}
+      <Header title="Joskun" />
+      <Header title={"joskun"} />
+      <ul>
+        {names.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+      <button onClick={handleClick}>Like({likes})</button>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(app);
-root.render(<h1>Develop. Preview. Ship.</h1>);
+root.render(<HomePage />);
